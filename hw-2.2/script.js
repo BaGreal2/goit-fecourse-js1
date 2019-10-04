e5bd545775f8c39cec268b6f753fb3101b95dbcf
BaGreal2 <artemonik17@gmail.com>
2019-10-04 19:50:12 +0300

    homework_2.2-done

--- hw-2.2/script.js
+++ hw-2.2/script.js
@@ -2,9 +2,27 @@ const passwords = ['qwerty', '111qwe', '123123', 'r4nd0mp4zzw0rd'];
 
 let attempts = 3;
 
-let attempsActive = 0;
+let attemptsActive = 1;
 
-// do {
-//     console.log(1);
+let attemptsLeft = 3;
 
-// } while (attempsActive <= attempts)
+do {
+    attemptsActive++;
+    let input = prompt(`Введите пароль...`)
+    attemptsLeft -= 1;
+    for (let i = 0; i <= passwords.length; i++) {
+        if (input === passwords[i]) {
+            alert('Добро Пожаловать!');
+            break;
+        } else if (input === null) {
+            alert('Отменено пользователем');
+            break;
+        } else if (input !== passwords[i] && attemptsLeft !== 0){
+            alert('Неверный пароль, у Вас осталось ' + attemptsLeft + ' попыток!');
+            break;
+        } else if (attemptsLeft === 0) {
+            alert('У вас закончились попытки, Ваш аккаунт заблокирован!')
+            break;
+        }
+    };
+} while (attemptsActive <= attempts);
